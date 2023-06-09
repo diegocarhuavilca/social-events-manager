8d2afd310a0771cf1ebd4d974320bc63de9f0e47
Diego.Carhuavilca <diego.carhuavilca@gmail.com>
2023-06-09 00:40:06 -0500

    feat(Firebase):Upload Image Service

--- src/main/java/com/socialEventManager/backEnd/services/FirebaseService.java
+++ src/main/java/com/socialEventManager/backEnd/services/FirebaseService.java
@@ -6,7 +6,16 @@ import com.google.cloud.storage.BlobInfo;
 import com.google.firebase.cloud.StorageClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -20,12 +29,24 @@ public class FirebaseService {
     }
 
     public String uploadImage(MultipartFile imageFile) throws IOException {
+        File convertedFile = convertMultipartFileToFile(imageFile);
+
         String imageName = generateUniqueImageName();
-        BlobInfo blobInfo = storageClient.bucket("jarvin-3cce1.appspot.com").create(imageName, imageFile.getBytes());
+
+        BlobInfo blobInfo = storageClient.bucket("jarvin-3cce1.appspot.com").create(imageName, Files.readAllBytes(convertedFile.toPath()),"image/png");
         return blobInfo.getMediaLink();
     }
 
     private String generateUniqueImageName() {
         return UUID.randomUUID().toString();
     }
+
+    private File convertMultipartFileToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
+
 }
